771c6ada28486319b3b935609821fa74029d0864
Massy100 <massielle.coti@gmail.com>
2024-04-23 09:59:11 -0600

    primer commit

---
+++ arbol_binario/src/main/java/com/mycompany/arbol_binario/Arbol_binario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arbol_binario;
+
+/**
+ *
+ * @author Massielle
+ */
+public class Arbol_binario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
